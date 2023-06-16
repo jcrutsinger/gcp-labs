33730b6179050e6c0669fffbff13fa11d7ef66e4
student-lab-14 <student-lab-14@datacouchtrainingoutlook.onmicrosoft.com>
2023-06-16 21:51:06 +0000

    updated

--- june16-challenge/start/frontend/quiz/gcp/storage.py
+++ june16-challenge/start/frontend/quiz/gcp/storage.py
@@ -11,18 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-apiVersion: v1
-kind: Service
-metadata:
-  name: quiz-frontend
-  labels:
-    app: quiz-app
-    tier: frontend
-spec:
-  type: LoadBalancer
-  ports:
-  - port: 80
-    targetPort: http-server
-  selector:
-    app: quiz-app
-    tier: frontend
+
+import os
+project_id = os.getenv('GCLOUD_PROJECT')
+bucket_name = os.getenv('GCLOUD_BUCKET')
+
+from google.cloud import storage
+
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(bucket_name)
+
+"""
+Uploads a file to a given Cloud Storage bucket and returns the public url
+to the new object.
+"""
+def upload_file(image_file, public):
+    blob = bucket.blob(image_file.filename)
+
+    blob.upload_from_string(
+        image_file.read(),
+        content_type=image_file.content_type)
+
+    if public:
+        blob.make_public()
+
+    return blob.public_url
